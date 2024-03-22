58c506dd13d3b2e966caf570020213518ca35952
Sevastian Bajenov <sevabaj@gmail.com>
2024-03-22 11:59:57 +0200

    Implement basic argument parsing.

--- go2web.py
+++ go2web.py
@@ -1,1 +1,12 @@
-print('hello')
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", help="Make an HTTP request to the specified URL and print the response")
+    parser.add_argument("-s", nargs='+', help="Make an HTTP request to search the term using your favorite search engine and print top 10 results")
+
+    args = parser.parse_args()
+    print(args.s)
+
+if __name__ == "__main__":
+    main()
