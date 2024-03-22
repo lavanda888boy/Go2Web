f58f638b0bb19431573904cf85032cede483ec12
Sevastian Bajenov <sevabaj@gmail.com>
2024-03-22 12:17:30 +0200

    Implement input url parsing.

--- go2web.py
+++ go2web.py
@@ -1,9 +1,20 @@
 import argparse
+from urllib.parse import urlparse
 import socket
 import ssl
 
 HTTPS_PORT = 443
 HTTP_PORT = 80
+
+def parse_url(url):
+    parsed_url = urlparse(url)
+
+    scheme = parsed_url.scheme
+    host = parsed_url.netloc
+    path = parsed_url.path
+
+    return scheme, host, path
+
 
 def send_http_get_request(host, port, path):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,14 +39,21 @@ def send_http_get_request(host, port, path):
 
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-u', help='Make an HTTP request to the specified URL and print the response')
-    # parser.add_argument('-s', nargs='+', help='Make an HTTP request to search the term using your favorite search engine and print top 10 results')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', help='Make an HTTP request to the specified URL and print the response')
+    parser.add_argument('-s', nargs='+', help='Make an HTTP request to search the term using your favorite search engine and print top 10 results')
 
-    # args = parser.parse_args()
-    # print(args.s)
+    args = parser.parse_args()
 
-    print(send_http_get_request('utm.md', HTTPS_PORT, '/'))
+    if args.u:
+        scheme, host, path = parse_url(args.u)
+        if scheme == 'https':
+            print(send_http_get_request(host, HTTPS_PORT, path))
+        else:
+            print(send_http_get_request(host, HTTP_PORT, path))
+    else:
+        pass
+
 
 if __name__ == "__main__":
     main()
